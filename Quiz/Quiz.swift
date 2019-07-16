c2dbe50d587f6d17493b4430a1f1e922a2320ab5
Garoto0518 <gabrielrguez0518@gmail.com>
2019-07-16 10:49:17 -0700

    Added new questions and fix code to work with objects

--- Quiz/Quiz.swift
+++ Quiz/Quiz.swift
@@ -7,24 +7,32 @@ class Quiz{
 private let questions: [String] = [
     "What is the captial of Colorado?",
     "Who coined the term 'debugging'?",
-    "Below what temperature do mosquitos stop flying?"
+    "Below what temperature do mosquitos stop flying?",
+    "Answer to life and the universe as whole?",
+    "Best Movie?",
+    "Best TV Show?",
+    "Best Language"
 ]
     
 private let answers: [String] = [
     "Denver",
     "Grace Hopper",
-    "50 degrees F"
+    "50 degrees F",
+    "42",
+    "Interstellar",
+    "Brooklyn 99",
+    "Spanish"
 ]
 
     var count : Int {
         return questions.count
     }
     
-    func question( number num:Int ) -> String {
+    func question( _ num:Int ) -> String {
         return questions[num]
     }
     
-    func answer( number num:Int ) -> String {
+    func answer( _ num:Int ) -> String {
         return answers[num]
     }
 }
